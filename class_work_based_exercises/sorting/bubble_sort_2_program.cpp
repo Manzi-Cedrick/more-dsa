56de08636f08c58880f7162294d6d01fdc1410c2
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-17 16:13:15 +0200

    bubble sorting algorithm: initial commit

---
+++ class_work_based_exercises/sorting/bubble_sort_2_program.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iteration,i,j,temp,n=6;
+    int num[n] = {5,9,27,55,11,0};
+    cout<<"Unsorted array"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<num[i]<<"\t";
+    }
+    for(iteration =1; iteration<n;iteration++){
+        cout<<"\nIteration: "<<iteration<<endl;
+        for(int j=0;j<n-1;j++){
+            if(num[j+1]<num[j]){
+                temp =num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+            cout<<endl;
+            cout<<"J = "<<j<<endl;
+            for(int k=0;k<n;k++){
+                cout<<num[k]<<"\t";
+            }
+        cout<<endl;
+        }
+    }
+    cout<<"\nThe sorted array"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<num[i]<<"\t";
+    }
+}
