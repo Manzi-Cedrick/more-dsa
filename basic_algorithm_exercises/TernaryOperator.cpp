037efee8d622c58878c802c979eeb541eae56851
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-17 16:09:54 +0200

    ternary operator functionality

---
+++ basic_algorithm_exercises/TernaryOperator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1=5 ,n2=10,max;
+    max = (n1 >n2) ? n1 : n2;
+    cout<<"The Larget Number between "<<n1<<"and "<<n2<<"is"<<max;
+    return 0;
+}
