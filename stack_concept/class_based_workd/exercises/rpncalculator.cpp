4bc7b9bbf75c82dc28b0211d2b9ebb8cd2eacddf
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-05-15 20:27:18 +0200

    [feat]: reverse polish notation assignment evaluation

---
+++ stack_concept/class_based_workd/exercises/rpncalculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int evaluateReversePN(string *tokens, int size) {
+    stack<int> s;
+    for (int i = 0; i < size; i++) {
+        string token = tokens[i];
+        if (token == "+") {
+            int y = s.top(); s.pop();
+            int x = s.top(); s.pop();
+            s.push(x + y);
+        } else if (token == "*") {
+            int y = s.top(); s.pop();
+            int x = s.top(); s.pop();
+            s.push(x * y);
+        } else if (token == "-") {
+            int y = s.top(); s.pop();
+            int x = s.top(); s.pop();
+            s.push(x - y);
+        } else if (token == "/") {
+            int y = s.top(); s.pop();
+            int x = s.top(); s.pop();
+            s.push(x / y);
+        } else {
+            s.push(stoi(token));
+        }
+    }
+    return s.top();
+}
+
+int main() {
+    string tokens1[] = {"2","1","+","3","*"};
+    string tokens2[] = {"4","13","5","/","+"};
+    int size1 = sizeof(tokens1)/sizeof(tokens1[0]);
+    int size2 = sizeof(tokens2)/sizeof(tokens2[0]);
+    cout << evaluateReversePN(tokens1, size1) << endl;
+    cout << evaluateReversePN(tokens2, size2) << endl;
+    return 0;
+}
